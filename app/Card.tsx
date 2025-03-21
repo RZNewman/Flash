e786bca676ae03a688dbf90fd08b92010cf20493
RZNewman <ruznewman@gmail.com>
2025-03-21 17:35:58 -0500

    feat: translate action cost

--- app/Card.tsx
+++ app/Card.tsx
@@ -5,6 +5,29 @@ type CardProps = {
   abil: Ability;
 };
 
+function translateActionCost(a: string) {
+  switch (a.toLowerCase()) {
+    case "1":
+      return "1 Action";
+    case "1a":
+      return "1 Action Attack";
+    case "0":
+      return "On your turn";
+    case "f":
+      return "Fast";
+    case "s":
+      return "Slow";
+    case "r":
+      return "Reaction";
+    case "c":
+      return "Colossal";
+    case "i":
+      return "Instant";
+    case "amod":
+      return "On any attack";
+  }
+}
+
 export default function Card(props: CardProps) {
   const { abil } = props;
   return (
@@ -13,12 +36,12 @@ export default function Card(props: CardProps) {
         {abil["Name"]}
       </Title>
       <Text fs="italic" size="s" pl="sm">
-        {abil["Sub list"]}
+        {abil["Sub list"]} {abil.Tier}
       </Text>
 
       {abil["Action Cost"] && (
         <Text span fw={700} pr="sm">
-          {abil["Action Cost"]}:
+          {translateActionCost(abil["Action Cost"])}:
         </Text>
       )}
       <Text span>{abil.Notes}</Text>
