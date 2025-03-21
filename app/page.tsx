7f8a483596f54ff1e3b8f18edf66f5f0028fe569
RZNewman <ruznewman@gmail.com>
2025-03-21 13:44:04 -0500

    chore: parse

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,27 @@ import { useState } from "react";
 import Papa from "papaparse";
 
 export default function Home() {
-  const [abilityDB, setAbilityCSV] = useState([]);
+  const [abilityDB, setAbilityCSV] = useState<Record<string, any>[]>([]);
 
   fetch("./Abil.csv")
     .then((response) => response.text())
     .then((responseText) => {
-      const csv = Papa.parse(responseText, {
+      const csv = Papa.parse<Record<string, any>>(responseText, {
         header: true,
       });
       const parsedData = csv?.data;
-      const rows = Object.keys(parsedData[0]);
+      // const rows = Object.keys(parsedData[0]);
 
-      const columns = Object.values(parsedData[0]);
-      const res = rows.reduce((acc, e, i) => {
-        return [...acc, [[e], columns[i]]];
-      }, []);
-      console.log(res);
-      setAbilityCSV(res);
+      // const columns = Object.values(parsedData[0]);
+
+      // const abilities:Record<string,any>[] = [];
+      // rows.forEach((e, i) => {
+      //   const abil = {}
+
+      //   abilities.push(([e], columns[i]));
+      // });
+      console.log(parsedData);
+      setAbilityCSV(parsedData);
     });
 
   return (
