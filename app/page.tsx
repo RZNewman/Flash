1047784234e9f4885cea00c8baff915bbf543a9e
RZNewman <ruznewman@gmail.com>
2025-03-21 13:30:49 -0500

    chore: bs 4

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,26 @@
 
 import { useState } from "react";
 import Papa from "papaparse";
-import AbilityCSV from "./Abil.csv";
 
 export default function Home() {
   const [abilityDB, setAbilityCSV] = useState([]);
 
-  const reader = new FileReader();
+  fetch("./Abil.csv")
+    .then((response) => response.text())
+    .then((responseText) => {
+      const csv = Papa.parse(responseText, {
+        header: true,
+      });
+      const parsedData = csv?.data;
+      const rows = Object.keys(parsedData[0]);
 
-  // Event listener on reader when the file
-  // loads, we parse it and set the data.
-  reader.onload = async ({ target }) => {
-    if (!target) return;
-    if (!target.result) return;
-    const csv = Papa.parse(target.result, {
-      header: true,
+      const columns = Object.values(parsedData[0]);
+      const res = rows.reduce((acc, e, i) => {
+        return [...acc, [[e], columns[i]]];
+      }, []);
+      console.log(res);
+      setAbilityCSV(res);
     });
-    const parsedData = csv?.data;
-    const rows = Object.keys(parsedData[0]);
-
-    const columns = Object.values(parsedData[0]);
-    const res = rows.reduce((acc, e, i) => {
-      return [...acc, [[e], columns[i]]];
-    }, []);
-    console.log(res);
-    setAbilityCSV(res);
-  };
-  reader.readAsText(AbilityCSV);
 
   return (
     <main>
