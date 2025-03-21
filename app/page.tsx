269e753a502365631e9713ff16e960d0d972e52d
RZNewman <ruznewman@gmail.com>
2025-03-21 15:41:23 -0500

    feat: clean keys

--- app/page.tsx
+++ app/page.tsx
@@ -13,19 +13,16 @@ export default function Home() {
         const csv = Papa.parse<Record<string, any>>(responseText, {
           header: true,
         });
-        const parsedData = csv?.data;
-        // const rows = Object.keys(parsedData[0]);
-
-        // const columns = Object.values(parsedData[0]);
-
-        // const abilities:Record<string,any>[] = [];
-        // rows.forEach((e, i) => {
-        //   const abil = {}
-
-        //   abilities.push(([e], columns[i]));
-        // });
-        console.log(parsedData);
-        setAbilityCSV(parsedData);
+        const abilities = csv?.data;
+        abilities.forEach((abil) => {
+          Object.keys(abil).forEach((prop) => {
+            if (abil[prop] == "") {
+              delete abil[prop];
+            }
+          });
+        });
+        console.log(abilities);
+        setAbilityCSV(abilities);
       });
   }, []);
 
@@ -34,7 +31,7 @@ export default function Home() {
       <div>Next.js on GitHub Pages test</div>
       {abilityDB.map((e, i) => (
         <div key={i} className="item">
-          {e[0]}:{e[1]}
+          {e["Name"]}:{e["List"]}/{e["Sub List"]}
         </div>
       ))}
     </main>
